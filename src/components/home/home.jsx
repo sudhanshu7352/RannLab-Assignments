80bc13ab8bbd7e10ac583d0316863774c68bc174
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-07-05 16:46:33 +0530

    done

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,35 +1,41 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./home.css";
 export const Home = () => {
-  const [details, setDetails] = useState({ stock: "", start: "", end: "" });
-
+  const [details, setDetails] = useState({ stock: "TSLA", start: "2021-07-22", end: "2021-07-22" });
+  const [data,setData]=useState({})
   const handleChange = (e) => {
     //  let {id,value} =e.target
     setDetails({ ...details, [e.target.id]: e.target.value });
   };
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(details);
-
-    // axios
-    //   .post("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=Goh_dEjRv8pKt3UI6NqbS5JNvOhzBdwl", details)
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     alert("Register Successful");
-    //     setUserDetails({
-    //       email: "",
-    //       name: "",
-    //       phone: "",
-    //       password: "",
-    //     });
-    //     // navigate("/login");
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     alert("Register Failed");
-    //   });
+    console.log(details,"det");
+     getData()
   };
+  
+  const getData =()=>{
+    if(!details){
+      setDetails({ stock: "TSLA", start: "2021-07-22", end: "2021-07-23"})
+    }
+    axios
+    .get(`https://api.polygon.io/v2/aggs/ticker/${details.stock}/range/1/day/${details.start}/${details.end}?adjusted=true&sort=asc&limit=120&apiKey=Goh_dEjRv8pKt3UI6NqbS5JNvOhzBdwl`)
+    .then((res) => {
+      console.log(res.data,"helo");
+      setData(res.data)
+      // alert("Register Successful");
+     setDetails({ stock: "", start: "", end: "" })
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Register Failed");
+    });
+   
+  }
+  
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <div className="main_div">
       <div id="upper_div">
@@ -37,7 +43,7 @@ export const Home = () => {
         <form onSubmit={handleSubmit}>
           <div style={{ display: "flex" }}>
             <span>
-              <h2>Stock name : </h2>
+              <h2>Stock Tickel Name : </h2>
             </span>
             <span>
               <input
@@ -58,7 +64,7 @@ export const Home = () => {
                 type="text"
                 id="start"
                 className="inp"
-                placeholder="yyyy-mm-dd"
+                placeholder="  yyyy-mm-dd"
                 onChange={handleChange}
               />
             </span>
@@ -72,7 +78,7 @@ export const Home = () => {
                 type="text"
                 id="end"
                 className="inp"
-                placeholder="yyyy-mm-dd"
+                placeholder=" yyyy-mm-dd"
                 onChange={handleChange}
               />
             </span>
@@ -82,13 +88,24 @@ export const Home = () => {
           <input type="submit" id="button" value="Submit" />
         </form>
       </div>
-      <div>
-        {/* {
-            details.map((e)=>(
-                <h1>Stock name :{e.ticker}</h1>
-            ))
-        } */}
+      {data.results?
+     (<div >
+            <h1 className="stock">Stock Details</h1>
+             
+              <div  style={{ display: "flex" }}><span><h2>Stock name :</h2></span><span className="stock_Det">{data.ticker}</span></div>
+              <div  style={{ display: "flex" }}><span><h3>Trading Volume :</h3></span><span className="stock_Det">{data.results[0].v}</span></div>
+              <div  style={{ display: "flex" }}><span><h3>Lowest Price :</h3></span><span className="stock_Det">{data.results[0].l} $</span></div>
+              <div  style={{ display: "flex" }}><span><h3>Highest Price:</h3></span><span className="stock_Det">{data.results[0].h} $</span></div>
+              <div  style={{ display: "flex" }}><span><h3> Number of transcations:</h3></span><span className="stock_Det">{data.results[0].n}</span></div>
+      </div>)
+      : (
+       <>
+         <div>Search</div>
+       </>
+
+      )
+
+      }
       </div>
-    </div>
   );
 };
